c1e7496a7c47ae1a0848bde5b4109eb699f4ac7a
nickk19 <nikko8204@gmail.com>
2022-06-20 15:36:41 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# list-check
+
+At the moment, in the first iteration the program takes a .txt file as an argument and asks the user for a number to search. 
+Then the program proceeeds to scan the document and reports to the user if the number is present.
---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::io::{self, Write, BufReader, prelude::*};
+use std::fs::File;
+use std::env;
+
+fn read (input: &mut String) {
+    let _ = io::stdout().flush();
+    io::stdin()
+        .read_line(input)
+        .expect("Failed to read from stdin");
+}
+
+fn main() {
+    let command_line_args: Vec<String> = env::args().collect();
+    if command_line_args.len() != 2 {
+        panic!("Need to specify the fileName")
+    }
+
+    let file_name = &command_line_args[1];
+    let file = File::open(file_name).expect("Cannot open the file");
+    let reader = BufReader::new(file);
+
+    print!("Search for a number: ");
+    let mut number = String::new();
+    read(&mut number);
+    let number:u32 = number.trim().parse().expect("Not a valid number");
+
+    let mut counter:u8 = 0;
+
+    for line in reader.lines() {
+        let line = line.unwrap().parse::<u32>().unwrap();
+        if number == line {
+            counter+=1;
+        }
+    }
+
+    if counter > 0 {
+        println!("The number {} was found {} time(s)", number, counter);
+    } else {
+        println!("The number {} wasn't found", number);
+    }
+}
