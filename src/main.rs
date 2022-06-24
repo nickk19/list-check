670bb14ae4b38bf6b47108c4e25c64b091c169c7
nickk19 <nikko8204@gmail.com>
2022-06-24 12:45:26 +0200

    Improved grammar

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,17 @@ fn read (input: &mut String) {
 fn main() {
     let command_line_args: Vec<String> = env::args().collect();
     if command_line_args.len() != 2 {
-        panic!("Need to specify the name of the file")
+        panic!("Need to specify the name of the file.")
     }
 
     let file_name = &command_line_args[1];
-    let file = File::options().read(true).write(true).open(file_name).expect("Cannot open the file");
+    let file = File::options().read(true).write(true).open(file_name).expect("Cannot open the file.");
     let reader = BufReader::new(file);
 
     print!("Search for a number: ");
     let mut number = String::new();
     read(&mut number);
-    let number:u32 = number.trim().parse().expect("Not a valid number");
+    let number:u32 = number.trim().parse().expect("Not a valid number.");
 
     let mut counter:u8 = 0;
 
@@ -34,8 +34,8 @@ fn main() {
     }
 
     if counter > 0 {
-        println!("The number {} was found {} time(s)", number, counter);
+        println!("The number {} was found {} time(s).", number, counter);
     } else {
-        println!("The number {} wasn't found", number);
+        println!("The number {} wasn't found.", number);
     }
 }
